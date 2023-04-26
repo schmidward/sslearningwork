bf8398f80cdaa9f26bff5b8f7b1d1979640b93da
schmidward <eric.d.schmid1@gmail.com>
2023-04-26 18:25:33 -0500

    ProjectSecurityConfig updates

--- src/main/java/com/ericdschmid/SecurityBackendApplication/config/ProjectSecurityConfig.java
+++ src/main/java/com/ericdschmid/SecurityBackendApplication/config/ProjectSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.ericdschmid.SecurityBackendApplication.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
