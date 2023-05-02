e903ba4a8d72a2a46417488acad7c2b737700fda
schmidward <eric.d.schmid1@gmail.com>
2023-05-02 18:40:14 -0500

    Created UsernamePwdAuthenticationProvider

---
+++ src/main/java/com/ericdschmid/SecurityBackendApplication/config/SecurityUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,51 @@
+package com.ericdschmid.SecurityBackendApplication.config;
+
+import com.ericdschmid.SecurityBackendApplication.model.Customer;
+import com.ericdschmid.SecurityBackendApplication.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecurityUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        //Load User Details from the storage system
+        //Compare the passwords
+        //Create an authentication object that says if it's successful or not
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if (customer.size() > 0) {
+            if (passwordEncoder.matches(pwd, customer.get(0).getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid credentials");
+            }
+        } else {
+            throw new BadCredentialsException("No User registered with these details.");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
