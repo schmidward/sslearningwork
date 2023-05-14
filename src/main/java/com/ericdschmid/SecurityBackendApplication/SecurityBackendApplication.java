7c4bd65121b718f39f8dfc73181e3980c75758ae
Eric D. Schmid <eric.d.schmid1@gmail.com>
2023-05-14 17:48:28 -0500

    adding debug mode

--- src/main/java/com/ericdschmid/SecurityBackendApplication/SecurityBackendApplication.java
+++ src/main/java/com/ericdschmid/SecurityBackendApplication/SecurityBackendApplication.java
@@ -2,8 +2,10 @@ package com.ericdschmid.SecurityBackendApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true) //NOT RECOMMENDED FOR PRODUCTION
 public class SecurityBackendApplication {
 
 	public static void main(String[] args) {
